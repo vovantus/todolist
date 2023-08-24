74bd06b018156908441f19de8626d7e2a5995062
vovantus <vshirokov@gmail.com>
2023-08-24 20:42:19 +0200

    fix after sync 25 08

--- src/App.js
+++ src/App.js
@@ -2,104 +2,103 @@ import { useState, useEffect } from "react";
 //import { db, fireApp, auth } from './firebase.js';
 //import { collection, addDoc, query, getDocs } from 'firebase/firestore';
 
+function ShowAddTaskField({ addTask }) {
+  const [value, setValue] = useState("");
 
-function ShowAddTaskField({addTask}){
-
-  function getTaskText () {
-    const taskInput = document.getElementById('taskInput');
-    const text = taskInput.value;
-    taskInput.value = "";
-    return text;
-  }
-
-  function getGetEnterPressed (e) {
+  function getGetEnterPressed(e) {
     if (e.keyCode === 13) {
-      const text = e.target.value;
-      e.target.value = "";
-      return(text);
-    }
-    return '';
+      addTask(value);
+      setValue("");
+    }   
   }
 
   return (
-            <div>
-              <input type="text" placeholder="Введите текст задачи" id="taskInput" onKeyDown={(e) => addTask(getGetEnterPressed(e))}></input>
-              <button onClick={() => addTask(getTaskText())}>Добавить</button>
-            </div>
-          )
-
+    <>
+      <div>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          placeholder="Введите текст задачи"
+          id="taskInput"
+          onKeyDown={getGetEnterPressed}
+        />
+        <button
+          onClick={() => {
+            addTask(value);
+            setValue("");
+          }}
+        >
+          Добавить
+        </button>
+      </div>
+    </>
+  );
 }
 
-function ShowTaskList({tasks, toggleTask, deleteTask, swapTasks, updateTask}) {
+function ShowTaskList({
+  tasks,
+  toggleTask,
+  deleteTask,
+  swapTasks,
+  updateTask
+}) {
+  // DONE todo сделать через useState
+  const [draggedTaskId, setDraggedTaskId] = useState(null);
 
-  let draggedTaskId;
-  
   function showDelPrompt(id) {
-    const userDecision = window.confirm('Удалить задачу?');
+    const userDecision = window.confirm("Удалить задачу?");
     if (userDecision) {
       deleteTask(id);
     }
-    return;
   }
 
   function showEditPrompt(id, text) {
-    const userDecision = prompt('Измените текст задания:',text);
+    const userDecision = prompt("Измените текст задания:", text);
     if (userDecision) {
       updateTask(id, userDecision);
     }
-    return;
   }
 
   function onDragOver(e) {
     e.preventDefault();
   }
 
-  function replaceTaskOnDrop(e,targetTaskId) {
-    e.preventDefault();
-    swapTasks(targetTaskId,draggedTaskId);
+  if (tasks.length === 0) {
+    return <div>Список задач пуст!</div>;
   }
 
-  function getGragedTask(id) {
-    draggedTaskId = id;
-  }
-
-  if (tasks.length===0) {
-    return (<>
-            <div>
-              Список задач пуст!
-              </div>
-      </>)
-  }
-
-
-  const tasksList = tasks.map((el,id) => {
+  const tasksList = tasks.map((el, id) => {
     return (
-      <li key={'task' + id} className={ el.open ? 'open' : 'closed' } onDragStart={() => getGragedTask(id)} onDragOver = {onDragOver} onDrop={(e)=>replaceTaskOnDrop(e,id)} draggable>
+      <li
+        key={"task" + id}
+        className={el.open ? "open" : "closed"}
+        onDragStart={() => setDraggedTaskId(id)}
+        onDragOver={onDragOver}
+        onDrop={() => swapTasks(id, draggedTaskId)}
+        draggable
+      >
         <div className="task">
           <div>
-            <input type="checkbox" checked={!el.open} onChange={()=> toggleTask(id)}></input>
+            <input
+              type="checkbox"
+              checked={!el.open}
+              onChange={() => toggleTask(id)}
+            ></input>
             {el.text}
           </div>
-          <div  className="taskButtons">
-            <button onClick={()=> showEditPrompt(id,el.text)}>Изменть</button>
-            <button onClick={()=> showDelPrompt(id)}>Удалить</button>
+          <div className="taskButtons">
+            <button onClick={() => showEditPrompt(id, el.text)}>Изменть</button>
+            <button onClick={() => showDelPrompt(id)}>Удалить</button>
           </div>
         </div>
       </li>
-    )
-  }
-  );
-  return (
-            <ul>
-              {tasksList}
-              </ul>          
-          );
-};
-
-
+    );
+  });
+  return <ul>{tasksList}</ul>;
+}
 
 export default function ToDoList() {
-
   const [tasks, setTasks] = useState(getTasks());
 
   function toggleTaskState(id) {
@@ -109,37 +108,47 @@ export default function ToDoList() {
   }
 
   function addTask(text) {
-    if (text === '') {
+    if (text.trim() === "") {
       return;
-    };
+    }
     const newTask = new SingleTask(text, true);
-    const updatedTasks = tasks.slice();
-    updatedTasks.unshift(newTask);
-    setTasks(updatedTasks);
+    setTasks([newTask,...tasks]);
   }
 
   function delTask(id) {
-    const updatedTasks = tasks.slice();
-    updatedTasks.splice(id,1);
+     // DONE tood реализовать удаление через filter()
+    const updatedTasks = tasks.filter((el,index) => (index!==id));
     setTasks(updatedTasks);
   }
 
   function changeTaskPosition(position, id) {
-    const moveUp = id > position;    
-    const swaptask = tasks[id];
-    const updatedTasks = [];
-    for (let i=0; i<tasks.length; i++) {
-      if (i===id) {
-        continue;
-      }
-      if (moveUp && i === position) {
-        updatedTasks.push(swaptask);
-      }
-      updatedTasks.push(tasks[i]);
-      if (!moveUp && i === position){
-        updatedTasks.push(swaptask);
-      }
+     // DONE todo драг-н-дроп на себя же
+    if (position === id) {
+      return;
     }
+
+    // DONE todo переписать на слайсы
+    const updatedTasks = id > position ?
+      [...tasks.slice(0,position), tasks[id],...tasks.slice(position,id),...tasks.slice(id+1)]
+      :
+      [...tasks.slice(0,id),...tasks.slice(id+1,position+1), tasks[id],...tasks.slice(position+1)];
+
+    // const updatedTasks = [];
+    // const moveUp = id > position;
+    // const swaptask = tasks[id];
+    // for (let i = 0; i < tasks.length; i++) {
+    //   if (i === id) {
+    //     continue;
+    //   }
+    //   if (moveUp && i === position) {
+    //     updatedTasks.push(swaptask);
+    //   }
+    //   updatedTasks.push(tasks[i]);
+    //   if (!moveUp && i === position) {
+    //     updatedTasks.push(swaptask);
+    //   }
+    // }
+
     setTasks(updatedTasks);
   }
 
@@ -149,35 +158,36 @@ export default function ToDoList() {
     setTasks(updatedTasks);
   }
 
-
-  return (<>
-            <h1>TODO List</h1>
-            <ShowAddTaskField addTask={addTask}/>
-            <ShowTaskList tasks={tasks} toggleTask={toggleTaskState} deleteTask={delTask} swapTasks={changeTaskPosition} updateTask={updateTask}/>
-         </>)
-
+  // prettier
+  return (
+    <>
+      <h1>TODO List</h1>
+      <ShowAddTaskField addTask={addTask} />
+      <ShowTaskList
+        tasks={tasks}
+        toggleTask={toggleTaskState}
+        deleteTask={delTask}
+        swapTasks={changeTaskPosition}
+        updateTask={updateTask}
+      />
+    </>
+  );
 }
 
 class SingleTask {
-  constructor (text, open) {
-    return {
-      'text' : text,
-      'open' : open,
-    }    
+  constructor(text, open) {
+    this.text = text;
+    this.open = open;
   }
 }
 
-class Tasks {
-  constructor (tasks) {
-    return tasks.map((el)=>{
-      return new SingleTask(el, true)
-    })
-  }
+function getTasks() {
+  const mockTasks = [
+    "!Mockup task 1: create todo",
+    "!Mockup task 2: call accountant",
+    "!Mockup task 3: connect backend"
+  ];
+
+  // DONE todo через map
+  return mockTasks.map(el => new SingleTask(el, true));
 }
-
-function getTasks () {
-  const mockTasks = ['!Mockup task 1: create todo','!Mockup task 2: call accountant','!Mockup task 3: connect backend'];
-  return new Tasks(mockTasks)
-}
-
-
